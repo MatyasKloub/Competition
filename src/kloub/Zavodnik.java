16d55b87ccc9595f5d0c2a2c4d2dcc3fb71ca5b8
MatyasKloub <matyas.kloub@tul.cz>
2022-03-30 15:54:15 +0200

    init

--- src/kloub/Zavodnik.java
+++ src/kloub/Zavodnik.java
@@ -2,19 +2,20 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package zavod_zavodnik;
+package kloub;
 
 /**
  *
  * @author matya
  */
-public class zavod {
+public class Zavodnik {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        
     }
     
 }
